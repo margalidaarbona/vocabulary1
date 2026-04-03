4765d84ac26046cee1845b5795f5094196fcee94
Margalidaarbona <margalida.arbona@pm.sfassis.org>
2026-04-03 18:18:08 +0200

    Arreglar carga de estilos Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Añadimos Tailwind aquí
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/vocabulary1/', // <-- AÑADE ESTA LÍNEA (con las barras a los lados)
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Y lo activamos aquí
+  ],
+  base: '/vocabulary1/', 
 })
